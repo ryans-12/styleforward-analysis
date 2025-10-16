5c1a71131259ee6a478b12eb4aa6750802e4cc04
Ryan <rstack312@gmail.com>
2025-10-16 16:15:04 -0400

    got map working

--- pages/Executive_Summary.py
+++ pages/Executive_Summary.py
@@ -2,7 +2,12 @@ import streamlit as st
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+import plotly.express as px
 from data.snowflake_conn import import_to_pandas
+
+st.set_page_config(
+    layout="wide",
+)
 
 
 transactions_df = import_to_pandas("SELECT * FROM transactions")
@@ -31,6 +36,49 @@ def display_channel_pie():
     ax.set_title('Sales by Channel', fontsize=32, weight='bold')
     st.pyplot(fig, width = "stretch")
 
+transactions_joined_stores = import_to_pandas("SELECT transactions.TRANSACTION_ID, transactions.STORE_ID, Stores.City, Stores.State, transactions.TOTAL_AMOUNT from transactions INNER JOIN Stores on Transactions.STORE_ID = Stores.Store_Id")
+
+def display_stores_map():
+    total_sales_by_store = transactions_joined_stores.groupby(['STORE_ID', 'CITY','STATE'])['TOTAL_AMOUNT'].sum().reset_index()
+    # Rename the column for clarity
+    total_sales_by_store.rename(columns={'TOTAL_AMOUNT': 'TOTAL_SALES'}, inplace=True)
+
+    
+    # Dictionary of coordinates keyed by city name
+    coordinates = {
+        'Austin': {'lat': 30.2711286, 'lon': -97.7436995},
+        'Dallas': {'lat': 32.7762719, 'lon': -96.7968559},
+        'Houston': {'lat': 29.7589382, 'lon': -95.3676974},
+        'San Antonio': {'lat': 29.4246002, 'lon': -98.4951405},
+        'Fort Worth': {'lat': 32.753177, 'lon': -97.3327459},
+        'El Paso': {'lat': 31.7601164, 'lon': -106.4870404},
+        'Plano': {'lat': 33.0136764, 'lon': -96.6925096},
+    }
+
+    
+    # Map coordinates to the DataFrame
+    total_sales_by_store['latitude'] = total_sales_by_store['CITY'].map(lambda city: coordinates[city]['lat'])
+    total_sales_by_store['longitude'] = total_sales_by_store['CITY'].map(lambda city: coordinates[city]['lon'])
+
+    total_sales_by_store['TOTAL_SALES'] = total_sales_by_store['TOTAL_SALES'].astype(float)
+
+   # Create scatter map
+    fig = px.scatter_map(
+        total_sales_by_store,
+        lat="latitude",
+        lon="longitude",
+        size="TOTAL_SALES",
+        hover_name="CITY",
+        hover_data={"STATE": True, "TOTAL_SALES": True},
+        color_discrete_sequence=["red"],
+        zoom=4,
+        height=400
+    )
+
+    fig.update_layout(mapbox_style="MapboxBasic")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    st.plotly_chart(fig)
+
 
 
 
@@ -58,6 +106,6 @@ with r1c2:
 st.text("")
 st.text("")
 with r2c1:
-    display_channel_pie()
+    display_stores_map()
 
 
