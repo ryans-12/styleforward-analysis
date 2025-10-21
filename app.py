60e0527758de2c33c1c84234782ae7bfd468363d
Ryan <rstack312@gmail.com>
2025-10-21 12:14:10 -0400

    changed file slash

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
 
-pg = st.navigation([st.Page("pages\Executive_Summary.py"), st.Page("pages\Customer_Touchpoint_Analysis.py"),st.Page("pages\Marketing_Budget_Analysis.py"), st.Page("pages\Sales.py")])
+pg = st.navigation([st.Page("pages/Executive_Summary.py"), st.Page("pages/Customer_Touchpoint_Analysis.py"),st.Page("pages/Marketing_Budget_Analysis.py"), st.Page("pages/Sales.py")])
 pg.run()
 
